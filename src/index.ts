18f072d92cb4f36c9c35127c3508990b65cdb5cf
gabiinDev <gabriel@gabrieldiazdev.com>
2022-09-18 18:34:48 -0300

    fix bad modules

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,3 @@ export { default as MyButton } from './components/MyButton.vue';
 
 export { HTTPBaseService} from './classes/api/http-base-service';
 export type { IVueComponent, IVueController, IVueModel } from "./interfaces/Vue";
-
-module.exports = "gss-art-common-lib";
